7b389865adf23903c1c8cab511725b3f1bcb2ba9
KoshiSG4 <gunasinghekoshi@gmail.com>
2026-03-20 12:38:21 +0530

    Modified bq_collector to retrieve Cloud Build, Cloud Run and Kubernetes Data

--- bigquery/bq_collector.py
+++ bigquery/bq_collector.py
@@ -13,31 +13,38 @@ def fetch_logs(start_time: str, end_time: str):
 
     dataset_reference = "prj-datasource-billing.ds_billing"
 
-    queries = {
-        "billing_summary_daily":f"""
-            SELECT *
-            FROM `{dataset_reference}.billing_summary_daily`
-            WHERE usage_date = DATE '{datetime.fromisoformat(start_time).date()}'
-        """,
+    platforms = ["Cloud Build", "Cloud Run", "Kubernetes Engine"]
 
-        "cloud_pricing_export":f"""
-            SELECT *
-            FROM `{dataset_reference}.cloud_pricing_export`
-            WHERE export_time >= TIMESTAMP('{datetime.fromisoformat(start_time).replace(tzinfo=timezone.utc)}')
-            AND export_time < TIMESTAMP('{datetime.fromisoformat(end_time).replace(tzinfo=timezone.utc)}')
-        """,
-    }
+    # Convert time
+    start_ts = datetime.fromisoformat(start_time).replace(tzinfo=timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
+    end_ts = datetime.fromisoformat(end_time).replace(tzinfo=timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
 
-    for table in billing_export_tables:
-        queries[table] = f"""
-            SELECT *
-            FROM `{dataset_reference}.{table}`
-            WHERE  usage_start_time>= TIMESTAMP('{datetime.fromisoformat(start_time).replace(tzinfo=timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")}')
-            AND usage_end_time < TIMESTAMP('{datetime.fromisoformat(end_time).replace(tzinfo=timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")}')
-        """
+    for platform in platforms:
+        platform_key = platform.replace(" ", "_").lower()
+        all_data[platform_key] = []
 
-    for name, query in queries.items():
+        for table in billing_export_tables:
+            query = f"""
+                SELECT
+                    service.description AS service_name,
+                    sku.description AS sku_name,
+                    usage_start_time,
+                    usage_end_time,
+                    project.id AS project_id,
+                    location,
+                    cost,
+                    currency,
+                    transaction_type
+                FROM `{dataset_reference}.{table}`
+                WHERE service.description = "{platform}"
+                    AND usage_start_time >= TIMESTAMP('{start_ts}')
+                    AND usage_end_time < TIMESTAMP('{end_ts}')
+            """
+
         data = run_query(query)
-        all_data[name] = data
+        all_data[platform_key].extend(data)
+  
 
     return all_data
+
+
