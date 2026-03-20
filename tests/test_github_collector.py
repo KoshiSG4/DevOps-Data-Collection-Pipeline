ec81e000cd3ec1803c0fdad3f39a69bc535a34ef
KoshiSG4 <gunasinghekoshi@gmail.com>
2026-03-20 20:32:03 +0530

    Modified test_github_collector to get values from config file

--- tests/test_github_collector.py
+++ tests/test_github_collector.py
@@ -1,4 +1,5 @@
 import sys
+import yaml
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -6,11 +7,22 @@ from collectors.github_collector import fetch_commits
 from collectors.github_collector import fetch_prs
 from collectors.github_collector import fetch_issues
 
-commits = fetch_commits("ivedha-tech","platformnex-frontend")
-print(f"Commits: {commits}")
+config_path = os.path.join(os.path.dirname(__file__), "..", "config", "config.yaml")
 
-prs = fetch_prs("ivedha-tech","platformnex-frontend")
-print(f"Pull Requests: {prs}")
+with open(config_path, "r") as f:
+    config = yaml.safe_load(f)
 
-issues = fetch_issues("ivedha-tech","platformnex-frontend")
-print(f"Issues: {issues}")
+cicds = config["cicd_collector"]
+
+for cicd in cicds:
+    owner = cicd["owner"]
+    repo = cicd["repo"]
+
+    commits = fetch_commits("ivedha-tech","platformnex-frontend")
+    print(f"Commits ({repo}): {commits}")
+
+    prs = fetch_prs("ivedha-tech","platformnex-frontend")
+    print(f"Pull Requests ({repo}): {prs}")
+
+    issues = fetch_issues("ivedha-tech","platformnex-frontend")
+    print(f"Issues ({repo}): {issues}")
